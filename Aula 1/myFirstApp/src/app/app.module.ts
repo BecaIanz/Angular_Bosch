c7a60a79cca47773705d0577e45b31a1c08743f6
BecaIanz <silveiraianzr@gmail.com>
2026-03-13 14:39:54 -0300

    initialization first project in angular

---
+++ Aula 1/myFirstApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InputComponent } from './componentes/input/input.component';
+import { ButtonComponent } from './componentes/button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
